d1716c3068431d835bc842b63a1cee1021fd1a51
Serg Pershin <pershin@exbico.ru>
2019-09-09 23:50:29 +0500

    menu acco added

--- burgers/js/main.js
+++ burgers/js/main.js
@@ -8,12 +8,12 @@ $(document).ready(function () {
     hamburgerMenuLink.on('click', function () {
         $('#hamburgerMenu').show();
     });
-    
+
     let ingredientsCloseIcon = $('#ingredientsCloseIcon');
     ingredientsCloseIcon.on('click', function () {
         $('#ingredients').hide();
     });
-    
+
     let compound = $('#compound');
     compound.on('click', function () {
         $('#ingredients').show();
@@ -23,5 +23,18 @@ $(document).ready(function () {
         speed: 300,
         addClasses: true,
         easing: 'swing'
+    });
+
+    let menuAccoTrigger = $('.menu-acco__trigger');
+    menuAccoTrigger.on('click', function () {
+        let self = $(this);
+        $('.menu-acco__item').each(function (index, elem) {
+            let menuAccoItem = $(elem);
+            if (menuAccoItem.hasClass('active')) {
+                menuAccoItem.removeClass('active');
+            }
+        });
+        self.parents('.menu-acco__item').toggleClass('active');
+        return false;
     });
 });
